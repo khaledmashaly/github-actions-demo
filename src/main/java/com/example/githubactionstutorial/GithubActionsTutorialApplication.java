781ae4963d5ac969cfca49ca6f145890f037c3c3
Khaled Mashaly <kmmashaly@gmail.com>
2021-02-05 16:59:16 +0200

    Create a demo for GitHub Actions

---
+++ src/main/java/com/example/githubactionstutorial/GithubActionsTutorialApplication.java
@@ -0,0 +1,23 @@
+package com.example.githubactionstutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+public class GithubActionsTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionsTutorialApplication.class, args);
+	}
+
+}
+
+@RestController
+@RequestMapping("/")
+class HomeController {
+	@GetMapping
+	public String getHome() {
+		return "Hello, World!";
+	}
+}
---
+++ src/test/java/com/example/githubactionstutorial/GithubActionsTutorialApplicationTests.java
@@ -0,0 +1,24 @@
+package com.example.githubactionstutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.beans.factory.annotation.Autowired;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class GithubActionsTutorialApplicationTests {
+	@Autowired
+	MockMvc mockMvc;
+
+	@Test
+	void returnsHelloWorld() throws Exception {
+		mockMvc.perform(get("/"))
+			.andExpect(status().isOk())
+			.andExpect(content().string("Hello, World!"));
+	}
+}
